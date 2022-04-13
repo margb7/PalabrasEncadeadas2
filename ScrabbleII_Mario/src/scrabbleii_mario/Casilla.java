f6a476c2de8a333f28e04e2150f209611c7f3b1b
margb7 <mariogonzalezgoian@gmail.com>
2022-04-13 17:59:05 +0200

    Javadoc Casilla

--- ScrabbleII_Mario/src/scrabbleii_mario/Casilla.java
+++ ScrabbleII_Mario/src/scrabbleii_mario/Casilla.java
@@ -26,10 +26,19 @@ public class Casilla {
         this.tipo = tipo;
     }
 
+    /**
+     * Getter do contido da casilla
+     * @return o valor. Pode ser unha letra ou unha cadea vacía no caso de ser unha casilla vacía 
+     * ou casilla especial.
+     */
     public String getContido() {
         return contido;
     }
 
+    /**
+     * Getter do multiplicador da casilla.
+     * @return o valor do multiplicador
+     */
     public byte getMultiplicador() {
         
         byte out;
@@ -47,18 +56,36 @@ public class Casilla {
         return out;
     }
 
+    /**
+     * Getter para saber de que tipo é a casilla.
+     * @return <ul>
+     * <li>0 para unha casilla vacía</li>
+     * <li>1 para unha casilla X2</li>
+     * <li>2 para unha casilla X3</li>
+     * <li>3 para unha casilla X4</li>
+     * <li>4 para unha casilla de valor dobre de palabra</li>
+     * </ul>
+     */
     public byte getTipo() {
         return tipo;
     }
 
+    /**
+     * Método para saber se a casilla é especial.
+     * @return true se se trata dunha casilla especial.
+     */
     public boolean eEspecial() {
         return tipo != 0;
     }
 
-    public boolean estaVacia() {
-        return contido.equals("") && tipo == 0;
-    }
-
+    /**
+     * Devolve a representación da casilla para mostrala no taboleiro.
+     * @return <ul>
+     *  <li>"  " se é unha casilla vacía</li>
+     *  <li>"vv" se non é unha casilla vacía ou unha casilla especial (vv representa o valor da casilla)</li>
+     *  <li>"x2", "x3", "x4" ou "2P" se é un multiplicador.</li> 
+     * </ul>
+     */
     public String valorMostra() {
         String out = "";
 
